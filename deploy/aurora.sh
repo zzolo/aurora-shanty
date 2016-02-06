7a70ab025693a08e856cbfc16960a804c60d2d14
Alan Palazzolo <github@zzolo.org>
2016-02-06 00:29:22 -0600

    Updating to use x11 vnc.

--- deploy/aurora.sh
+++ deploy/aurora.sh
@@ -15,7 +15,11 @@ export DISPLAY=:1
 PROCESSING_CMD=/usr/local/bin/processing-java
 
 # Start VNC.  We want to make sure this happens
-/home/pi/aurora-shanty/deploy/tightvnc.sh > /home/pi/aurora.log 2>&1;
+# /home/pi/aurora-shanty/deploy/tightvnc.sh > /home/pi/aurora.log 2>&1;
+
+# Start X11VNC
+x11vnc > /home/pi/x11vnc.log 2>&1;
+
 
 # Start sketch
 $PROCESSING_CMD --sketch=/home/pi/aurora-shanty/aurora_sketch --output=/home/pi/aurora-shanty/aurora_output --force --run
